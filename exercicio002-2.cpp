9be9decdc7721eb3f06a385a2c1fc4d2cd998839
IsaqueZaratustra <ADSisaque@gmail.com>
2022-05-15 18:06:58 -0300

    add

---
+++ exercicio002-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    std::cout 
+    << "Vou" << "\n"
+    << "aprender" << "\n"
+    << "a" << "\n"
+    << "programar"<< "\n"
+    << "na" << "\n"
+    << "linguagem" << "\n"
+    << "C++"<< "\n";
+    system("Pause");
+    return 0;
+}

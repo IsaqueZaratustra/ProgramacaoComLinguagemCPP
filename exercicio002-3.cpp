a5c12e234933f59683d1625bb7275fa4103982b9
IsaqueZaratustra <ADSisaque@gmail.com>
2022-05-16 16:47:45 -0300

    add

---
+++ exercicio002-3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+// Isaque Pereira Marques da Silva
+int main(){
+    std::cout 
+    << "Vou" << "\n"
+    << "aprender" << "\n"
+    << "a" << "\n"
+    << "programar"<< "\n"
+    << "na" << "\n"
+    << "linguagem" << "\n"
+    << "C++"<< "\n";
+    system("Pause");
+    return 0;
+}
+/* int main(){
+    std::cout 
+    << "Vou" << "\n"
+    << "aprender" << "\n"
+    << "a" << "\n"
+    << "programar"<< "\n"
+    << "na" << "\n"
+    << "linguagem" << "\n"
+    << "C++"<< "\n";
+    system("Pause");
+    return 0;
+}*/

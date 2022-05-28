9410694130c096bcaf078f004e19ecc56ddde970
IsaqueZaratustra <ADSisaque@gmail.com>
2022-05-28 18:26:15 -0300

    add

---
+++ exercicios/exercicio003.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+int NumComputadores = 100;
+int SerialComputadores = 11111;
+
+int main()
+{
+    std::cout << "$$$$$$$$$$" << std::endl;
+    std::cout << "Valor de NumComputadores: " << NumComputadores << std::endl;
+    std::cout << "Valor de SerialComputadores: " << SerialComputadores << std::endl;
+    std::cout << "$$$$$$$$$$" << std::endl;
+    system("Pause");
+    return 0;
+}

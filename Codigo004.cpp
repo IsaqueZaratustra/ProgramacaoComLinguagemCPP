e31b94065a9293140a51e23d1fa259046db6eb35
IsaqueZaratustra <ADSisaque@gmail.com>
2022-05-29 16:41:25 -0300

    rev

--- Codigo004.cpp
+++ Codigo004.cpp
@@ -7,9 +7,9 @@ int main()
     const int NUM_VIDAS = 10;
     int TotalDeVidas;
     TotalDeVidas = NUM_VIDAS - 1;
-    std::cout << "Total de Vidas" << TotalDeVidas << std::endl;
-    std::cout << "Valor constante NUM_VIDAS" << NUM_VIDAS << std::endl;
-    std::cout << " Endereço de memória NUM_VIDA" << & NUM_VIDAS << std::endl;
+    std::cout << "Total de Vidas " << TotalDeVidas << std::endl;
+    std::cout << "Valor constante de NUM_VIDA " << NUM_VIDAS << std::endl;
+    std::cout << " Endereço de memória de NUM_VIDA " << & NUM_VIDAS << std::endl;
     system("Pause");
     return 0;
 

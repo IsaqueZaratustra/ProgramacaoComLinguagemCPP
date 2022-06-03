a07c97d921090ba920b4156b5dddf7ac6540f355
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-03 18:54:59 -0300

    rev

--- Codigo006.cpp
+++ Codigo006.cpp
@@ -3,6 +3,10 @@
 int main()
 {
     int Numero01, Numero02;
+    int Soma = Numero01 + Numero02;
+    int Subtracao = Numero01 - Numero02;
+    int Multiplacao = Numero01 * Numero02;
+    int Divisao = Numero01 / Numero02;
 
     std::cout << "Digite o primeiro numero: ";
     std::cin >> Numero01 ;
@@ -10,9 +14,11 @@ int main()
     std::cout << "Digite o segundo numero: ";
     std::cin >> Numero02 ;
     std::cout << "Numero02 = " << Numero02 << std::endl; 
-    int Soma = Numero01 + Numero02;
+
     std::cout << "Numero01 + Numero02 = " << Soma << std::endl; 
-    
+    std::cout << "Numero01 - Numero02 = " << Subtracao << std::endl; 
+    std::cout << "Numero01 * Numero02 = " << Multiplacao << std::endl; 
+    std::cout << "Numero01 / Numero02 = " << Divisao << std::endl; 
 
     system("Pause");
     return 0;

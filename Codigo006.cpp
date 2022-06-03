f459fd280ed924088d1531a41e42b2af6429a6ed
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-03 17:51:25 -0300

    inc

---
+++ Codigo006.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int Numero01, Numero02;
+    std::cout << "Digite o Numero01: ";
+    std::cin >> Numero01 >>std::endl;
+    std:cin >> Numero02>> Numero01 >> std::endl;
+    system("Pause");
+    return 0;
+}

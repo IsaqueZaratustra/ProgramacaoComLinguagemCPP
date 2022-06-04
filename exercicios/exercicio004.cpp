dc091729e798912fda7625054273e05ac81ef3cc
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-04 18:51:40 -0300

    add int

--- exercicios/exercicio004.cpp
+++ exercicios/exercicio004.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-    float Numero01;
-    float Numero02;
-    float Numero03;
-    float Soma = Numero01 + Numero02 + Numero03;
-    float Media = Soma / 3; 
+    int Numero01;
+    int Numero02;
+    int Numero03;
+    int Soma = Numero01 + Numero02 + Numero03;
+    int Media = Soma / 3; 
 
     std::cout << "Digite o primeiro numero: ";
     std::cin >> Numero01 ;
@@ -14,7 +14,8 @@ int main()
     std::cin >> Numero02 ;
     std::cout << "Digite o terceiro numero: ";
     std::cin >> Numero03 ;
-    std::cout << "Numeros digitados: " << Numero01 << Numero02 << Numero03 << std::endl;
+    std::cout << "Numeros digitados: " << Numero01 << " " << Numero02 << " " << Numero03 << std::endl;
+    std::cout << "Soma dos numeros:" << Soma << std::endl;
     std::cout << "Media dos numeros: " << Media << std::endl;
 
     system("Pause");

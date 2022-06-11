d4a259a532afa9770216c2e75db1106f9f20645c
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-11 18:10:43 -0300

    add

---
+++ exercicios/exercicio005.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+int main()
+{
+    int Numero;
+
+    Numero=0;
+    while (Numero <=50){
+        std::cout << Numero << std::endl;
+        Numero++;
+    }
+
+    system("Pause");
+    return 0;
+}

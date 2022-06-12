14901eb2093f52924e0cda0c317da72ac88bbace
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-12 16:58:05 -0300

    add

---
+++ exercicios/exercicios007.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int Numero;
+
+    Numero=0;
+    while(Numero <=50){
+        if(Numero % 2 !=0)
+        {
+            std::cout << Numero << std::endl;
+        }
+        Numero++;
+    }
+
+    system("Pause");
+    return 0;
+}

0d9fcc075eaf96267d94ae34ac72104571e40516
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-14 17:21:15 -0300

    add

---
+++ exercicios/exercicio011.cpp
@@ -0,0 +1,42 @@
+# include <iostream>
+
+int adicao (int num1, int num2);
+int subtracao (int num1, int num2);
+int multiplicacao (int num1, int num2);
+int divisao (int num1, int num2);
+
+int main()
+{
+    int num1, num2;
+
+    std::cout << "insira dois numeros, para obter resultados das 4 operacoes basicas " << std::endl;
+    std::cout << "Insira o primeiro numero: " << std::endl;
+    std::cin >> num1;
+    std::cout << "Insira o segundo: " << std::endl;
+    std::cin >> num2;
+    std::cout << "Os numeros escolhidos foram " << num1 << " e " << num2 << std::endl;
+    std::cout << "Os resultados das quatro operacoes numericas foram:" << std::endl;
+    std::cout << "Adicao: " << adicao(num1, num2) << std::endl;
+    std::cout << "Subtracao: " << subtracao(num1, num2) << std::endl;
+    std::cout << "Multiplicacao: " << multiplicacao(num1, num2) << std::endl;
+    std::cout << "Divisao: " << divisao(num1, num2) << std::endl;
+
+    system("Pause");
+    return 0;
+}
+
+int adicao (int num1, int num2){
+    return num1 + num2;
+}
+
+int subtracao (int num1, int num2){
+    return num1 - num2;
+}
+
+int multiplicacao (int num1, int num2){
+    return num1 * num2;
+}
+
+int divisao (int num1, int num2){
+    return num1 / num2;
+}

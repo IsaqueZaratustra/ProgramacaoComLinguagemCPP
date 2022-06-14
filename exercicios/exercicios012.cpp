e967307cdb36ebcf6ffe5b10b495c47e680d7239
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-14 18:27:09 -0300

    add

---
+++ exercicios/exercicios012.cpp
@@ -0,0 +1,17 @@
+
+
+int adicao (int num1, int num2){
+    return num1 + num2;
+}
+
+int subtracao (int num1, int num2){
+    return num1 - num2;
+}
+
+int multiplicacao (int num1, int num2){
+    return num1 * num2;
+}
+
+int divisao (int num1, int num2){
+    return num1 / num2;
+}

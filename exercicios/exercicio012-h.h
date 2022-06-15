699ee74e5e75910f47c36cd49e447479b5459dd9
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-15 15:52:30 -0300

    add

--- exercicios/exercicio012-h.h
+++ exercicios/exercicio012-h.h
@@ -3,7 +3,7 @@
 int adicao (int num1, int num2);
 int subtracao (int num1, int num2);
 int multiplicacao (int num1, int num2);
-int divisao (int num1, int num2);
+float divisao (int num1, int num2);
 int potencia (int num1, int num2);
 int raizQuadrada (int num1);
 int raizQuadrada2 (int num2);
--- exercicios/exercicio012.cpp
+++ exercicios/exercicio012.cpp
@@ -12,8 +12,8 @@ int multiplicacao (int num1, int num2){
     return num1 * num2;
 }
 
-int divisao (int num1, int num2){
-    return num1 / num2;
+float divisao (int num1, int num2){
+    return (float)num1 / (float)num2;
 }
 
 int potencia (int num1, int num2){

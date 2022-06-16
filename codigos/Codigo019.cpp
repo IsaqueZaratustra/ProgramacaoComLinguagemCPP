ea65580895732cdad9da11de4ad36584d7d2a2bb
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-16 17:56:57 -0300

    add

--- codigos/Codigo019.cpp
+++ codigos/Codigo019.cpp
@@ -2,14 +2,18 @@
 
 int main()
 {
-    int Num1 = 0;
+    //*ptr = ponteiro, aquele cuja armazena um endereco
+    int Num1 = 0, *ptr;
     double Num2 = 000.000;
 
     std::cout << "Tamanho da variavel Num1: " << sizeof(Num1)<< " Bytes" << std::endl;
     std::cout << "Tamanho da variavel Num2: " << sizeof(Num2)<< " Bytes" << std::endl;
     std::cout << "Endereço de memoria da variavel Num1 :" << &Num1 << std::endl;
     std::cout << "Endereço de memoria da variavel Num2 :" << &Num2 << std::endl;
-
+    std::cout << "Tamanho da variavel ptr: " << sizeof(ptr)<< " Bytes" << std::endl;
+    std::cout << "Endereço de memoria da variavel ptr :" << &ptr << std::endl;
+    std::cout << "O que esta armazenado na variavel ptr :" << ptr << std::endl;
+   
     system("Pause");
     return 0;
 }

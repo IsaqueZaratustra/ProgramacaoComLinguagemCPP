ad88343f5ff9db4d68d0eb5c7addfaa90da8c938
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-16 17:32:33 -0300

    rev

--- codigos/Codigo019.cpp
+++ codigos/Codigo019.cpp
@@ -5,8 +5,8 @@ int main()
     int Num1 = 0;
     double Num2 = 000.000;
 
-    std::cout << "Tamanho da variavel Num1: " << sizeof(Num1) << std::endl;
-    std::cout << "Tamanho da variavel Num2: " << sizeof(Num2) << std::endl;
+    std::cout << "Tamanho da variavel Num1: " << sizeof(Num1)<< " Bytes" << std::endl;
+    std::cout << "Tamanho da variavel Num2: " << sizeof(Num2)<< " Bytes" << std::endl;
     std::cout << "Endereço de memoria da variavel Num1 :" << &Num1 << std::endl;
     std::cout << "Endereço de memoria da variavel Num2 :" << &Num2 << std::endl;
 

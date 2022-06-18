3f7c48488c755b60805b8eae7730d6bd3cca416c
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-18 17:47:10 -0300

    rev

--- exercicios/exercicio013.cpp
+++ exercicios/exercicio013.cpp
@@ -2,17 +2,18 @@
 
 int main()
 {
-    int Numero = 234;
-    int *Ponteiro;
-    int *OutroPtr;
+    int Numero = {234};
+    int* Ponteiro = &Numero;
+    int* OutroPtr = Ponteiro;
     *Ponteiro = 1456;
     Numero = *Ponteiro;
-    std::cout << *Ponteiro << std::endl;
-    std::cout << &Numero << std::endl;
-    std::cout << &Ponteiro << std::endl;
-    Numero = 1656;
-    *OutroPtr = Numero;
-    std::cout << *OutroPtr << std::endl;
+    std::cout <<"Valor de Numero: " << *Ponteiro << std::endl;
+    std::cout <<"Endereco da variavel Numero:" <<&Numero << std::endl;
+    std::cout <<"Endereco da variavel ponteiro: " << &Ponteiro << std::endl;
+    std::cout <<"Valor contido DENTRO de Ponteiro: " << Ponteiro << std::endl;
+    //*Ponteiro = *Ponteiro + 200;
+    *Ponteiro += 200;
+    std::cout <<"Valor de Numero: " <<*OutroPtr << std::endl;
 
     system("Pause");
     return 0;

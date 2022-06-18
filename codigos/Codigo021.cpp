dacb461e1dc29be5d9b948bba2c69c834343c6b4
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-18 16:14:46 -0300

    add

--- codigos/Codigo021.cpp
+++ codigos/Codigo021.cpp
@@ -6,6 +6,10 @@ int main()
     char Letra {'E'};
     void *ptrG;
 
+    std::cout << "endereco de letra via ptrG = " << &Letra << std::endl;
+    std::cout << "endereco de numero via ptrG = " << &Numero << std::endl;
+    std::cout << "endereco de ptrG via ptrG = " << &ptrG << std::endl;
+
     ptrG = &Letra;
     std::cout << "Valor de Letra via ptrG = " << *(char *)ptrG << std::endl;
     ptrG = &Numero;

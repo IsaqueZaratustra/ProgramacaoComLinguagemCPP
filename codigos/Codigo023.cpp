4fcfeed61c2be6f1279eb3e3c1e54f6b4b25d06f
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-20 16:06:15 -0300

    rev

--- codigos/Codigo023.cpp
+++ codigos/Codigo023.cpp
@@ -5,14 +5,15 @@ void TrocaNumero (int& Num1, int& Num2);
 int main()
 {
     int Numero1, Numero2;
-
+    std::cout << "***Antes da Troca***" << std::endl;
     std::cout <<"Digite o primeiro numero: "<< std::endl;
     std::cin>>Numero1;
     std::cout <<"Digite o segundo numero: "<< std::endl;
     std::cin>>Numero2;
+    std::cout << "***Depois da Troca***" << std::endl;
     TrocaNumero(Numero1, Numero2);
-    std::cout << "Numero 1 depois da troca: " << Numero1<< std::endl;
-    std::cout << "Numero 2 depois da troca: " << Numero2<< std::endl;
+    std::cout << "Primeiro numero depois da troca: " << Numero1<< std::endl;
+    std::cout << "Segundo numero depois da troca: " << Numero2<< std::endl;
 
     system("Pause");
     return 0;
@@ -24,7 +25,4 @@ void TrocaNumero (int& Num1, int& Num2){
     Num1 = Num2;
 
     Num2 = Temp;
-    
-    //std::cout << "Numero 1 depois da troca: " << Num1<< std::endl;
-    //std::cout << "Numero 2 depois da troca: " << Num2<< std::endl;
 }

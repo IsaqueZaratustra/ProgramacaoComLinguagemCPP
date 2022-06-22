1d44dca14227f823a6b4698310000a32613ccc22
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-22 15:44:18 -0300

    rev

--- exercicios/exercicio014.cpp
+++ exercicios/exercicio014.cpp
@@ -10,7 +10,7 @@ for (size_t i = 0; i < 10; i++)
     std::cout << "Digite um numero para acrescentar a soma: "<< Soma << std::endl;
     std::cin >> Numero[i];
     Soma = Soma + Numero[i];
-    system("CLS");
+    system("Clear || CLS");
 }
     Resulta = Soma;
     std::cout << "A soma total dos numeros resulta em: " << Resulta << std::endl;

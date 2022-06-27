2867b72cde21bc4f11e2c03120f9c93a81031919
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-27 15:53:09 -0300

    add

---
+++ exercicios/exercicio017.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int ArrayNegativoPositivo [5] [5]{
+        {0,1,1,1,1},
+        {-1,0,1,1,1},
+        {-1,-1,0,1,1},
+        {-1,-1,-1,0,1},
+        {-1,-1,-1,-1,0},
+};
+    for (size_t i = 0; i < 5; i++)
+    {
+        for (size_t j = 0; j < 5; j++)
+        {
+            std::cout << ArrayNegativoPositivo[i][j] << std::endl;
+        }
+        
+    }
+    
+    system("Pause");
+    return 0;
+}

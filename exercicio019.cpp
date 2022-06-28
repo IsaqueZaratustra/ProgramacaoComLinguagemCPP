3e64acfcee4b9df978d8ec3594c2c89f098a62ed
IsaqueZaratustra <ADSisaque@gmail.com>
2022-06-28 15:44:51 -0300

    rev

--- exercicio019.cpp
+++ exercicio019.cpp
@@ -5,9 +5,9 @@ int main(int argc, char const *argv[])
     float Notas [] {4.9, 9.5, 5.6, 2.5, 9.6, 6.4, 7.5, 9.8, 8.7, 10.0};
     float QuantidadeDeNotas {0.0}, Soma {0.0}, Media {0.0};
 
-    for(auto Notas : Notas)
+    for(auto Nota : Notas)
     {
-    Soma += Notas;
+    Soma += Nota;
     }
     
     QuantidadeDeNotas = sizeof(Notas) / sizeof(float);

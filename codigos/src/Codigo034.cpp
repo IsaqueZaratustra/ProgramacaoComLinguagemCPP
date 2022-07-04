64f9c11a9649297cfb7eeb36fb5ad217b8275f7b
IsaqueZaratustra <ADSisaque@gmail.com>
2022-07-04 16:36:37 -0300

    add

--- codigos/src/Codigo034.cpp
+++ codigos/src/Codigo034.cpp
@@ -6,9 +6,11 @@ bool Conta :: Sacar(double Valor){
     if (Saldo < Valor)
     {
     std::cout << "Saldo insuficiente! " << std::endl;
+    return false;
     }else
     {
     Saldo -= Valor;
+    return true;
     }
     std::cout << "Valor sacado: " << Valor << std::endl;    
 }
@@ -19,7 +21,27 @@ void Conta :: Depositar(double Valor){
 }
 
 void Conta :: Transferir(Conta &Destino, double Valor){
+    if (Saldo < Valor)
+	{
+		std::cout << "\nSaldo Insuficiente\n";
 
+	}
+	else
+	{
+		//Aqui Destino está com o objeto passado para a função
+		//Logo vai chamar o método Depositar desta função
+		Destino.Depositar(Valor);
+		//Depois de depositar você deve retirar este valor depositado do saldo
+		Saldo -= Valor;
+		std::cout << "\n*****Dados*****\n";
+		std::cout << "Titular: " << Destino.GetTitular() << "\n";
+		std::cout << "Banco: " << Destino.GetBanco() << "\n";
+		std::cout << "Agencia: " << Destino.GetAgencia() << "\n";
+		std::cout << "Conta: " << Destino.GetNumConta() << "\n";
+		std::cout << "Transferência Realizada com Sucesso!\n";
+		std::cout << "Seu Saldo Atual: R$" << ConsultarSaldo() << "\n";
+		
+	}
 }
 
 double Conta :: ConsultarSaldo(){

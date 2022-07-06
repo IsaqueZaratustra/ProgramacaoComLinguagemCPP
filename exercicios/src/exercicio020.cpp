1e95928f96050e8e3b5ae7d0ebeae49b835200e5
IsaqueZaratustra <ADSisaque@gmail.com>
2022-07-06 15:46:13 -0300

    add rev

--- exercicios/src/exercicio020.cpp
+++ exercicios/src/exercicio020.cpp
@@ -1,24 +1,21 @@
 # include <iostream>
 
-void DesenharPonto3D (Ponto3D Pontos);
-
 struct Ponto3D
 {
     int X;
     int Y;
     int Z;
     void DesenharPonto3D (Ponto3D Pontos){
-        std::cout << "X: " << Pontos.X << " Y: " << Pontos.Y << " Z: " << Pontos.Z ;
+        std::cout << "X: " << Pontos.X << " Y: " << Pontos.Y << " Z: " << Pontos.Z << std::endl;
     }
 };
-
 
 int main(int argc, char const *argv[])
 {
     Ponto3D PontoC;
-    PontoC.X;
-    PontoC.Y;
-    PontoC.Z;
+    PontoC.X = 3;
+    PontoC.Y = 4;
+    PontoC.Z = 5;
     PontoC.DesenharPonto3D(PontoC);
 
 
